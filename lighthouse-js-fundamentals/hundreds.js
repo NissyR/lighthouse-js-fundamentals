7f36e3f5cafb19d179123d7cbe32132db73aad61
NissyR <basaniswetha@gmail.com>
2021-09-16 14:04:47 +0000

    hundreds.js

---
+++ lighthouse-js-fundamentals/hundreds.js
@@ -0,0 +1,8 @@
+function howManyHundreds(num)  {
+    return Math.floor(num / 100);
+}
+console.log(howManyHundreds(850));
+console.log(howManyHundreds(894));
+console.log(howManyHundreds(520));
+console.log(howManyHundreds(99));
+console.log(howManyHundreds(0));
