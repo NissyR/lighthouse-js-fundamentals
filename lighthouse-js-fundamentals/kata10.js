d4db2504421e6ff556ae623d07e0d99e6657bbd8
NissyR <basaniswetha@gmail.com>
2021-09-20 18:04:00 +0000

    kata10.js

---
+++ lighthouse-js-fundamentals/kata10.js
@@ -0,0 +1,13 @@
+function multiplicationTable (maxValue) {
+    let result = '';
+    for(let i = 1; i <= maxValue; i++) {
+        for(let j = 0; j < maxValue; j++) {
+            result += i + (i*j) + ' ';
+        }
+        result += '\n';
+    }
+    return result;
+};
+console.log(multiplicationTable(1));
+console.log(multiplicationTable(5));
+console.log(multiplicationTable(10));
